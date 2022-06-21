75ef903479b8568b55f4fda4bada9711b5d920ec
Nestot Kenneth Aniar <ken.aniar.dev@gmail.com>
2022-06-21 08:41:04 +0800

    added pageview

--- pages/test.js
+++ pages/test.js
@@ -3,8 +3,7 @@ import ReactGA from "react-ga4";
 
 export default function Test() {
   useEffect(() => {
-    ReactGA.initialize("G-WDNY0C9LKJ");
-    ReactGA.send("pageview");
+    ReactGA.send({ hitType: "pageview", page: window.location.pathname });
   }, []);
 
   return <div>test</div>;
