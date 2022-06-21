f7e3c76d72c1d0a668a45f8533c7aa48891c2493
Nestot Kenneth Aniar <ken.aniar.dev@gmail.com>
2022-06-21 08:30:10 +0800

    fix error

--- pages/test.js
+++ pages/test.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import ReactGA from "react-ga4";
 
-export default function test() {
+export default function Test() {
   useEffect(() => {
     ReactGA.initialize("your GA measurement id");
     ReactGA.send("pageview");
