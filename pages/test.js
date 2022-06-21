553aac5d4f8a8ef2aa17871b6abf1daf4ed4c9f6
Nestot Kenneth Aniar <ken.aniar.dev@gmail.com>
2022-06-21 08:56:20 +0800

    added gtag

--- pages/test.js
+++ pages/test.js
@@ -1,10 +1,39 @@
 import React, { useEffect } from "react";
 import ReactGA from "react-ga4";
-
+import Script from "next/script";
 export default function Test() {
   useEffect(() => {
+    ReactGA.initialize("G-WDNY0C9LKJ");
     ReactGA.send({ hitType: "pageview", page: window.location.pathname });
   }, []);
 
-  return <div>test</div>;
+  function sendEvent() {
+    ReactGA.event({
+      category: "your category",
+      action: "your action",
+      label: "your label", // optional
+      value: 99, // optional, must be a number
+      nonInteraction: true, // optional, true/false
+      transport: "xhr", // optional, beacon/xhr/image
+    });
+  }
+
+  return (
+    <div>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-WDNY0C9LKJ"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-WDNY0C9LKJ');
+        `}
+      </Script>
+      <button onClick={sendEvent}>send event</button>
+    </div>
+  );
 }
