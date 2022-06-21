92d6dbadbf7cb60bea476c23a3dfb5080feacc5a
Nestot Kenneth Aniar <ken.aniar.dev@gmail.com>
2022-06-21 08:23:17 +0800

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,31 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }) {
+  // const router = useRouter();
+
+  // useEffect(() => {
+  //   const handleRouteChange = (url, { shallow }) => {
+  //     // REACTGA
+  //     // Send pageview with a custom path
+  //     ReactGA.send({ hitType: "pageview", page: "/my-path" });
+
+  //     console.log(
+  //       `App is changing to ${url} ${
+  //         shallow ? "with" : "without"
+  //       } shallow routing`
+  //     );
+  //   };
+
+  //   router.events.on("routeChangeComplete", handleRouteChange);
+
+  //   // If the component is unmounted, unsubscribe
+  //   // from the event with the `off` method:
+  //   return () => {
+  //     router.events.off("routeChangeComplete", handleRouteChange);
+  //   };
+  // }, []);
+
+  return <Component {...pageProps} />;
 }
-
-export default MyApp
