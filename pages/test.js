d08d0698ad91a19f5f18c055d57071c895b06992
Nestot Kenneth Aniar <ken.aniar.dev@gmail.com>
2022-06-21 08:28:47 +0800

    added ga

--- pages/test.js
+++ pages/test.js
@@ -1,5 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
+import ReactGA from "react-ga4";
 
 export default function test() {
+  useEffect(() => {
+    ReactGA.initialize("your GA measurement id");
+    ReactGA.send("pageview");
+  }, []);
+
   return <div>test</div>;
 }
