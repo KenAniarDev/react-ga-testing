4a975a36ec185acd31555cab15a6fa5f92bd3fc1
Nestot Kenneth Aniar <ken.aniar.dev@gmail.com>
2022-06-24 11:14:48 +0800

    added _ to action

--- pages/test.js
+++ pages/test.js
@@ -9,7 +9,7 @@ export default function Test() {
   function sendEvent() {
     ReactGA.event({
       category: "test",
-      action: "test action",
+      action: "test_action",
       label: "test label", // optional
       value: 99, // optional, must be a number
       nonInteraction: true, // optional, true/false
